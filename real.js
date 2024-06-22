108e2d476f413764d941d591f2194cc2b193ad8a
gyampson <sgyampoh005@st.ug.edu.gh>
2024-06-21 23:55:47 -0700

    good

---
+++ real.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const samson = () => {
+  return (
+    <View>
+      <Text>samson</Text>
+    </View>
+  );
+};
+
+export default samson;
+
+const styles = StyleSheet.create({});
