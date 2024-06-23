9060318996e0ea8d08dbe153af8633d0adb5934e
gyampson <sgyampoh005@st.ug.edu.gh>
2024-06-23 02:03:26 -0700

    same

---
+++ gyam.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const gyam = () => {
+  return (
+    <View>
+      <Text>gyam</Text>
+    </View>
+  );
+};
+
+export default gyam;
+
+const styles = StyleSheet.create({});
